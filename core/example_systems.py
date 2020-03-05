f3538903f4dd234cbfe69f2aafa41017d509eba7
Vineet Tiruvadi <virati@gmail.com>
2020-03-05 14:05:23 -0500

    added ipynb folders to gitignore but not working for some reason or another

--- core/example_systems.py
+++ core/example_systems.py
@@ -5,10 +5,20 @@ Created on Thu Mar  5 11:37:18 2020
 
 @author: virati
 """
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+def gen_lin_data(factor,noise,samples):
+    x = np.random.uniform(-10,10,size=(samples,))
+    y = factor * x + np.random.normal(0,noise,size=x.shape)
+    
+    return x,y
 
 def simple_eg(slope=1.0,noise=0.0,samples=100):
-    x = np.random.uniform(-10,10,size=(samples,))
-    y = slope * x + np.random.normal(0,noise,size=x.shape)
+    x,y = gen_lin_data(slope,noise,samples)
+    #x = np.random.uniform(-10,10,size=(samples,))
+    #y = slope * x + np.random.normal(0,noise,size=x.shape)
     
     plt.figure()
     plt.scatter(x,y)
@@ -16,6 +26,7 @@ def simple_eg(slope=1.0,noise=0.0,samples=100):
     plt.ylim(-10,10)
     plt.xlim(-10,10)
     plt.axis('off')
+    
     plt.legend(['Correlated','Uncorrelated'])
     corr_val = stats.pearsonr(x,y)
     plt.text(2,-10,s='Pearson: ' + str(corr_val[0]) + '\n p=' + str(corr_val[1]))
